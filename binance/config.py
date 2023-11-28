fb2676dc517c04626115c60e5b2f51e2424f2b69
Martin King <martinking1997@gmail.com>
2023-11-28 11:35:37 +0800

    Update config.py

--- binance/config.py
+++ binance/config.py
@@ -1,6 +1,6 @@
-API_KEY = 'oA429xxhjHMCrHY7USFz3wzgkGpN5kCpv2awjDS0byBFVSKzgFqhJc9rUSp96xNs'
-API_SECRET = '80ncVNYiGkCSm3SQDSWbjrSXMvhULLJA18nmowZ6YDtbV1JRvvsAy4G7dGCSVOoW'
-
+API_KEY = 'your api key from exchange'
+API_SECRET = 'your secret from exchange'
+#this is a sample, if not use PROXIES, just PROXIES= {}
 PROXIES = {
         'http': "socks5h://127.0.0.1:12345",
         'https': "socks5h://127.0.0.1:12345" }
